04382607230a5d981f47b39e01caf4fea01673af
chanhaPrk <qkrcksgk90@gmail.com>
2020-10-13 21:51:26 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ChanhaPrk/movie_app_2019/precache-manifest.a9e64ac34547e66ff1ec4999c90e76b0.js"
+  "/chanhaPrk/movie_app_2019/precache-manifest.35856713b3bd8908c71df861f6cc4f17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChanhaPrk/movie_app_2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chanhaPrk/movie_app_2019/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
